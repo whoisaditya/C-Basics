c09d6b48bbf4428997dbb03c0d0537cea92fe607
Aditya Mitra <77195215+whoisaditya@users.noreply.github.com>
2021-02-26 18:24:00 +0530

    Create Sum of Two Numbers.c

---
+++ Sum of Two Numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int
+main ()
+{
+  int a, b;
+  printf ("Enter Number 1: ");
+  scanf ("%d", &a);
+  printf ("Enter Number 2: ");
+  scanf ("%d", &a);
+  int c = a + b;
+  printf ("The sum is %d", c);
+  return 0;
+}
