e330d96215342321cd0a5a904b78c5be7be8881c
Aditya Mitra <77195215+whoisaditya@users.noreply.github.com>
2021-02-26 18:33:23 +0530

    Update Area of Circle.c

--- Area of Circle.c
+++ Area of Circle.c
@@ -4,8 +4,8 @@ int main ()
   float area, r;
   printf ("Enter Radius: ");
   scanf ("%f", &r);
-  area = 22.0 * r / 7.0;
-  printf ("The area is %f", area);
+  area = 22.0 * r *r/ 7.0;
+  printf ("The area is %.2f", area);
   return 0;
 }
 
